d77e50af17bd178d033432faf3e213d055933f00
Kateryna Berehovych <kasia-94@ukr.net>
2023-02-22 21:51:53 +0200

    Add Formik

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,3 +1,4 @@
+import { Formik } from 'formik';
 import {
   ButtonGoogle,
   ButtonGroup,
@@ -16,28 +17,34 @@ import { FcGoogle } from 'react-icons/fc';
 
 export const RegisterForm = () => {
   return (
-    <Form>
-      <Google>
-        <TitleGoogle>You can log in with your Google Account:</TitleGoogle>
-        <ButtonGoogle>
-          <FcGoogle size={18} />
-          <TextGoogle>Google</TextGoogle>
-        </ButtonGoogle>
-      </Google>
+    <>
+      <Formik>
+        <Form>
+          <Google>
+            <TitleGoogle>You can log in with your Google Account:</TitleGoogle>
+            <ButtonGoogle>
+              <FcGoogle size={18} />
+              <TextGoogle>Google</TextGoogle>
+            </ButtonGoogle>
+          </Google>
 
-      <Title>Or log in using an email and password, after registering:</Title>
-      <Label>
-        <LabelText>Email:</LabelText>
-        <Input type="text" name="email" placeholder="your@email.com" />
-      </Label>
-      <Label>
-        <LabelText>Password:</LabelText>
-        <Input type="text" name="password" placeholder="Password" />
-      </Label>
-      <ButtonGroup>
-        <ButtonLogin>Log in</ButtonLogin>
-        <ButtonReg>Registration</ButtonReg>
-      </ButtonGroup>
-    </Form>
+          <Title>
+            Or log in using an email and password, after registering:
+          </Title>
+          <Label>
+            <LabelText>Email:</LabelText>
+            <Input type="text" name="email" placeholder="your@email.com" />
+          </Label>
+          <Label>
+            <LabelText>Password:</LabelText>
+            <Input type="text" name="password" placeholder="Password" />
+          </Label>
+          <ButtonGroup>
+            <ButtonLogin>Log in</ButtonLogin>
+            <ButtonReg>Registration</ButtonReg>
+          </ButtonGroup>
+        </Form>
+      </Formik>
+    </>
   );
 };
