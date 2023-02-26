de9091e900fabef281993c2fc552765089ce74de
Nikita Sokolov <nsokolov0309@gmail.com>
2023-02-27 01:37:32 +0200

    add swager

--- src/pages/Reports/Reports.jsx
+++ src/pages/Reports/Reports.jsx
@@ -2,20 +2,22 @@ import BackgroundPrivateContainer from 'components/BackgroundPrivateContainer';
 import Balance from 'components/Balance';
 import Container from 'components/Container';
 import Graphic from 'components/Graphic';
+import Swager from 'components/Swager';
 import { IconLink, LinkToMain } from './Reports.styled';
 
 export default function Reports() {
   return (
     <BackgroundPrivateContainer>
       <Container>
-        {/* <h2>REPORTS</h2> */}
-
         <LinkToMain to="/main">
           <IconLink size={18} />
           Main Page
         </LinkToMain>
 
         <Balance />
+
+        <Swager />
+
         <Graphic />
       </Container>
     </BackgroundPrivateContainer>
