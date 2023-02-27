a33bddf0bb83bd4b25adf358f783510957fa9e92
belyy10 <100143159+belyy10@users.noreply.github.com>
2023-02-27 19:32:06 +0000

    Deploying to gh-pages from @ belyy10/Kapusta-Front@6e7c6745e4fe78c3ea2a2fcdecc179c7d20a8fee 🚀

---
+++ static/js/425.592b65e1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[425],{17425:function(t,a,e){e.d(a,{jwm:function(){return r}});var h=e(89983);function r(t){return(0,h.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"g",attr:{},child:[{tag:"path",attr:{fill:"none",d:"M0 0h24v24H0z"}},{tag:"path",attr:{d:"M3 12h4v9H3v-9zm14-4h4v13h-4V8zm-7-6h4v19h-4V2z"}}]}]})(t)}}}]);
+//# sourceMappingURL=425.592b65e1.chunk.js.map
