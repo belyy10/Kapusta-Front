386aa32141b17397be1d220af1bbe4e70cd2a498
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2023-02-27 04:04:55 +0100

    transaction operation

---
+++ src/redux/transactions/transactionsSlice.js
@@ -0,0 +1,101 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+    fetchUserTransactions,
+    addTransaction,
+    removeTransaction,
+    fetchSummaryExpenses,
+    fetchSummaryIncomes,
+    fetchReportExpenses,
+    fetchReportIncomes
+} from './transactionsOperations';
+
+
+const initialState = {
+    transactions: [],
+    summary: [],
+    reportsData: [],
+    type: 'all',
+    isLoading: false,
+    // date: newDate()
+};
+
+const transactionSlice = createSlice({
+    name: 'transaction',
+    initialState,
+    extraReducers: {
+        [fetchUserTransactions.pending]: state => {
+            state.isLoading = true;
+        },
+        [fetchUserTransactions.fulfilled]: (state, action) => {
+            state.transactions = action.payload.data;
+            state.isLoading = false;
+        },
+        [fetchUserTransactions.rejected]: state => {
+            state.isLoading = true;
+        },
+        [addTransaction.pending]: state => {
+            state.isLoading = true;
+        },
+        [addTransaction.fulfilled]: (state, action) => {
+            state.transactions = [action.payload.data, ...state.transactions];
+            state.isLoading = false;
+        },
+        [addTransaction.rejected]: state => {
+            state.isLoading = true;
+        },
+        [removeTransaction.pending]: state => {
+            state.isLoading = true;
+        },
+        [removeTransaction.fulfilled]: (state, action) => {
+            state.transactions = state.transactions?.filter(
+                tr => tr._id !== action.payload.data._id
+            );
+            state.isLoading = false;
+        },
+        [removeTransaction.rejected]: state => {
+            state.isLoading = false;
+        },
+        [fetchSummaryExpenses.pending]: state => {
+            state.isLoading = true;
+        },
+        [fetchSummaryExpenses.fulfilled]: (state, action) => {
+            state.summary = action.payload.data;
+            state.isLoading = false;
+        },
+        [fetchSummaryExpenses.rejected]: state => {
+            state.isLoading = false;
+        },
+        [fetchSummaryIncomes.pending]: state => {
+            state.isLoading = true;
+        },
+        [fetchSummaryIncomes.fulfilled]: (state, action) => {
+            state.summary = action.payload.data;
+            state.isLoading = false;
+        },
+        [fetchSummaryIncomes.rejected]: state => {
+            state.isLoading = false;
+        },
+        [fetchReportExpenses.pending]: state => {
+            state.isLoading = true;
+        },
+        [fetchReportExpenses.fulfilled]: (state, action) => {
+            state.reportsData = action.payload.data;
+            state.isLoading = false;
+        },
+        [fetchReportExpenses.rejected]: state => {
+            state.isLoading = false;
+        },
+        [fetchReportIncomes.pending]: state => {
+            state.isLoading = true;
+        },
+        [fetchReportIncomes.fulfilled]: (state, action) => {
+            state.reportsData = action.payload.data;
+            state.isLoading = false;
+        },
+        [fetchReportIncomes.rejected]: state => {
+            state.isLoading = false;
+        },
+    }
+});
+
+export const transactionReducer = transactionSlice.reducer;
