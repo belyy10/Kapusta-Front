4c4b8b969041b14d5fbd1f521a20059f83bb5edc
Nikita Sokolov <nsokolov0309@gmail.com>
2023-02-28 12:26:12 +0200

    add fetch for transactions

--- src/redux/transactions/transactionsOperations.js
+++ src/redux/transactions/transactionsOperations.js
@@ -6,9 +6,13 @@ export const changeType = createAction('transactions/changeType');
 
 export const fetchUserTransactions = createAsyncThunk(
   'transaction/fetchUserTransactions',
-  async (_, thunkAPI) => {
+  async ({ controller, type }, thunkAPI) => {
     try {
-      const { data } = await axios.get('/transaction/:type');
+      console.log(controller);
+
+      const { data } = await axios.get(`/transaction?type=${type}`, {
+        signal: controller.signal,
+      });
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -27,6 +31,7 @@ export const addTransaction = createAsyncThunk(
         `/transaction/${credentials.type}`,
         credentials
       );
+
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
--- src/redux/transactions/transactionsSlice.js
+++ src/redux/transactions/transactionsSlice.js
@@ -1,103 +1,102 @@
-import { createSlice } from "@reduxjs/toolkit";
-import FormatDate from "utils/FormatDate";
+import { createSlice } from '@reduxjs/toolkit';
+import FormatDate from 'utils/FormatDate';
 import {
-    fetchUserTransactions,
-    addTransaction,
-    removeTransaction,
-    fetchSummaryExpenses,
-    fetchSummaryIncomes,
-    fetchReportExpenses,
-    fetchReportIncomes
+  fetchUserTransactions,
+  addTransaction,
+  removeTransaction,
+  fetchSummaryExpenses,
+  fetchSummaryIncomes,
+  fetchReportExpenses,
+  fetchReportIncomes,
 } from './transactionsOperations';
 
-
 const initialState = {
-    transactions: [],
-    summaryExpenses: [],
-    summaryIncomes: [],
-    reportsData: [],
-    type: 'all',
-    isLoading: false,
-    date: FormatDate.getDateObj(new Date()),
+  transactions: [],
+  summaryExpenses: [],
+  summaryIncomes: [],
+  reportsData: [],
+  type: 'all',
+  isLoading: false,
+  date: FormatDate.getDateObj(new Date()),
 };
 
 const transactionSlice = createSlice({
-    name: 'transaction',
-    initialState,
-    extraReducers: {
-        [fetchUserTransactions.pending]: state => {
-            state.isLoading = true;
-        },
-        [fetchUserTransactions.fulfilled]: (state, action) => {
-            state.transactions = action.payload.data;
-            state.isLoading = false;
-        },
-        [fetchUserTransactions.rejected]: state => {
-            state.isLoading = true;
-        },
-        [addTransaction.pending]: state => {
-            state.isLoading = true;
-        },
-        [addTransaction.fulfilled]: (state, action) => {
-            state.transactions = [action.payload.data, ...state.transactions];
-            state.isLoading = false;
-        },
-        [addTransaction.rejected]: state => {
-            state.isLoading = true;
-        },
-        [removeTransaction.pending]: state => {
-            state.isLoading = true;
-        },
-        [removeTransaction.fulfilled]: (state, action) => {
-            state.transactions = state.transactions?.filter(
-                tr => tr._id !== action.payload.data._id
-            );
-            state.isLoading = false;
-        },
-        [removeTransaction.rejected]: state => {
-            state.isLoading = false;
-        },
-        [fetchSummaryExpenses.pending]: state => {
-            state.isLoading = true;
-        },
-        [fetchSummaryExpenses.fulfilled]: (state, action) => {
-            state.summaryExpenses = action.payload.data;
-            state.isLoading = false;
-        },
-        [fetchSummaryExpenses.rejected]: state => {
-            state.isLoading = false;
-        },
-        [fetchSummaryIncomes.pending]: state => {
-            state.isLoading = true;
-        },
-        [fetchSummaryIncomes.fulfilled]: (state, action) => {
-            state.summaryIncomes = action.payload.data;
-            state.isLoading = false;
-        },
-        [fetchSummaryIncomes.rejected]: state => {
-            state.isLoading = false;
-        },
-        [fetchReportExpenses.pending]: state => {
-            state.isLoading = true;
-        },
-        [fetchReportExpenses.fulfilled]: (state, action) => {
-            state.reportsData = action.payload.data;
-            state.isLoading = false;
-        },
-        [fetchReportExpenses.rejected]: state => {
-            state.isLoading = false;
-        },
-        [fetchReportIncomes.pending]: state => {
-            state.isLoading = true;
-        },
-        [fetchReportIncomes.fulfilled]: (state, action) => {
-            state.reportsData = action.payload.data;
-            state.isLoading = false;
-        },
-        [fetchReportIncomes.rejected]: state => {
-            state.isLoading = false;
-        },
-    }
+  name: 'transaction',
+  initialState,
+  extraReducers: {
+    [fetchUserTransactions.pending]: state => {
+      state.isLoading = true;
+    },
+    [fetchUserTransactions.fulfilled]: (state, action) => {
+      state.transactions.push(action.payload.transactions);
+      state.isLoading = false;
+    },
+    [fetchUserTransactions.rejected]: state => {
+      state.isLoading = true;
+    },
+    [addTransaction.pending]: state => {
+      state.isLoading = true;
+    },
+    [addTransaction.fulfilled]: (state, action) => {
+      state.transactions = [action.payload.data, ...state.transactions];
+      state.isLoading = false;
+    },
+    [addTransaction.rejected]: state => {
+      state.isLoading = true;
+    },
+    [removeTransaction.pending]: state => {
+      state.isLoading = true;
+    },
+    [removeTransaction.fulfilled]: (state, action) => {
+      state.transactions = state.transactions?.filter(
+        tr => tr._id !== action.payload.data._id
+      );
+      state.isLoading = false;
+    },
+    [removeTransaction.rejected]: state => {
+      state.isLoading = false;
+    },
+    [fetchSummaryExpenses.pending]: state => {
+      state.isLoading = true;
+    },
+    [fetchSummaryExpenses.fulfilled]: (state, action) => {
+      state.summaryExpenses = action.payload.data;
+      state.isLoading = false;
+    },
+    [fetchSummaryExpenses.rejected]: state => {
+      state.isLoading = false;
+    },
+    [fetchSummaryIncomes.pending]: state => {
+      state.isLoading = true;
+    },
+    [fetchSummaryIncomes.fulfilled]: (state, action) => {
+      state.summaryIncomes = action.payload.data;
+      state.isLoading = false;
+    },
+    [fetchSummaryIncomes.rejected]: state => {
+      state.isLoading = false;
+    },
+    [fetchReportExpenses.pending]: state => {
+      state.isLoading = true;
+    },
+    [fetchReportExpenses.fulfilled]: (state, action) => {
+      state.reportsData = action.payload.data;
+      state.isLoading = false;
+    },
+    [fetchReportExpenses.rejected]: state => {
+      state.isLoading = false;
+    },
+    [fetchReportIncomes.pending]: state => {
+      state.isLoading = true;
+    },
+    [fetchReportIncomes.fulfilled]: (state, action) => {
+      state.reportsData = action.payload.data;
+      state.isLoading = false;
+    },
+    [fetchReportIncomes.rejected]: state => {
+      state.isLoading = false;
+    },
+  },
 });
 
 export const transactionReducer = transactionSlice.reducer;
