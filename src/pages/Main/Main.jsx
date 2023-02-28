52f66d5bdba925c372f78b7b42ecc94c2818a28d
Nikita Sokolov <nsokolov0309@gmail.com>
2023-02-28 17:43:29 +0200

    fix

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -2,34 +2,9 @@ import BackgroundPrivateContainer from 'components/BackgroundPrivateContainer';
 import Balance from 'components/Balance';
 import Container from 'components/Container';
 import { IconLink, LinkReport } from './Main.styled';
-// import CreateExpenses from 'components/Expenses/Expenses';
 import TransactionsContainer from 'components/TransactionsContainer/TransactionsContainer';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-import { fetchUserTransactions } from 'redux/transactions/transactionsOperations';
 
 export default function Main() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    dispatch(
-      fetchUserTransactions({
-        type: 'expenses',
-        controller,
-      })
-    );
-    dispatch(
-      fetchUserTransactions({
-        type: 'incomes',
-        controller,
-      })
-    );
-
-    return () => controller.abort();
-  }, [dispatch]);
-
   return (
     <BackgroundPrivateContainer>
       <Container>
