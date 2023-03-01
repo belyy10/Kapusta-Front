4553b985e9a7f0815af1e96e32393d7d8dd893ef
Natali Poliakova <99250547+Nataliphoenix@users.noreply.github.com>
2023-03-01 20:13:52 +0300

    mob

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import { lazy, Suspense, useEffect } from 'react';
 import { RestrictedRoute } from './RestrictedRoute';
 import { PrivateRoute } from './PrivateRoute';
+import { useMedia } from 'hooks/useMedia';
 
 import CreateTransactions from './CreateTransaction/CreateTransaction';
 import { useDispatch } from 'react-redux';
@@ -12,7 +13,7 @@ const Header = lazy(() => import('./Header'));
 const Main = lazy(() => import('../pages/Main'));
 const Reports = lazy(() => import('../pages/Reports'));
 const Login = lazy(() => import('../pages/Login'));
-// const Mobile = lazy(() => import('../pages/Mobile'));
+const Mobile = lazy(() => import('../pages/Mobile'));
 
 export default function App() {
   const dispatch = useDispatch();
@@ -37,7 +38,7 @@ export default function App() {
     return () => controller.abort();
   }, [dispatch]);
 
-  // const isMobile = useMedia();
+  const { isMobile } = useMedia();
 
   return (
     <Suspense>
@@ -51,10 +52,16 @@ export default function App() {
           />
           <Route
             path="main"
-            element={<PrivateRoute component={<Main />} redirectTo="/login" />}
+            // element={<PrivateRoute component={<Main />} redirectTo="/login" />}
+            element={ isMobile ? <PrivateRoute path="transactions" component={<Mobile/>} redirectTo="/login" /> 
+            : <PrivateRoute component={<Main />} redirectTo="/login" />}
           >
+          
+            
             <Route path="expenses" element={<CreateTransactions />} />
             <Route path="incomes" element={<CreateTransactions />} />
+
+            
           </Route>
           <Route
             path="reports"
--- src/pages/Mobile/Mobile.jsx
+++ src/pages/Mobile/Mobile.jsx
@@ -1,29 +1,34 @@
-import { useEffect, useState } from 'react';
+import { 
+  // useEffect,
+   useState } from 'react';
 
-import {
-  Balance,
-  TransactionsContainer,
-  MobileTypeBtn,
-  TransactionListMobile,
-} from 'components';
+import Balance from 'components/Balance';
+import TransactionsContainer from 'components/TransactionsContainer';
+import TransactionListMobile from 'components/TransactionListMobile';
+import  MobileTypeBtn from 'components/MobileTypeBtn';
 
 import {
   MobileContainer,
   MobileInfo,
   LinkToTransaction,
   IconLink,
+IconLinkReport,
+LinkToReport,
+InputDate,
+  
 } from './Mobile.styled.jsx';
-import { changeType } from 'redux/transactions';
-import { useDispatch } from 'react-redux';
+// import { changeType } from 'redux/transactions';
+// import { useDispatch } from 'react-redux';
 
 export default function Mobile() {
   const [trForm, openTrForm] = useState(false);
+  const currentDate = new Date().toISOString().slice(0, 10);
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(changeType('all'));
-  }, []);
+  // useEffect(() => {
+  //   dispatch(changeType('all'));
+  // }, []);
 
   return (
     <>
@@ -36,9 +41,28 @@ export default function Mobile() {
               <IconLink size={18} />
               TO TRANSACTION
             </LinkToTransaction>
+            <LinkToReport to="/reports">
+          Reports
+          <IconLinkReport size={14} color=" #52555F" />
+        </LinkToReport>
 
             <Balance />
             {/* календарь */}
+            
+                <InputDate
+                  name="date"
+                  type="date"
+                  min="1920-01-01"
+                  max={currentDate}
+                  // onChange={event =>
+                  //   setFieldValue(
+                  //     'date',
+                  //     moment(event.target.value).format('YYYY-MM-DD')
+                  //   )
+                  // }
+                />
+          
+             
           </MobileInfo>
         )}
         <TransactionsContainer />
