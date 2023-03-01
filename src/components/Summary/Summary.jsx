b684c567f33514491567215b1f0f340e76abcda6
belyy10 <maximgyda10@icloud.com>
2023-03-01 10:43:48 +0200

    fix

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,34 +1,33 @@
-
 // import { useState } from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchSummaryExpenses } from 'redux/transactions/transactionsOperations';
-import { getSummaryExpenses } from 'redux/transactions/transactionsSelectors';
+import { selectSummaryExpenses } from 'redux/transactions/transactionsSelectors';
 import { SummaryBox, SummaryTitle, SummaryBody } from './Summary.styled';
 
 import SummaryBodyList from './SummaryList';
 
-
 export default function Summary() {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        dispatch(fetchSummaryExpenses());
-    }, [dispatch]);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchSummaryExpenses());
+  }, [dispatch]);
 
-    const monthSummary = useSelector(getSummaryExpenses);
-    // const type = useSelector(getType);
-    // console.log('type', type);
+  const monthSummary = useSelector(selectSummaryExpenses);
+  // const type = useSelector(getType);
+  // console.log('type', type);
 
-    return (
-        <SummaryBox>
-            <SummaryTitle>Summary</SummaryTitle>
+  return (
+    <SummaryBox>
+      <SummaryTitle>Summary</SummaryTitle>
 
-            <SummaryBody>
-                {/* {elements} */} 
-                {monthSummary && monthSummary.map(item => <SummaryBodyList key={item.id}
-                    item={item} />)}
- 
-            </SummaryBody>
-        </SummaryBox>
-    );
-};
+      <SummaryBody>
+        {/* {elements} */}
+        {monthSummary &&
+          monthSummary.map(item => (
+            <SummaryBodyList key={item.id} item={item} />
+          ))}
+      </SummaryBody>
+    </SummaryBox>
+  );
+}
