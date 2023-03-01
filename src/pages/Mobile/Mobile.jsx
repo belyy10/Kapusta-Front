23e0e2351ef7332102beab94a2fb54d362f0c03f
belyy10 <maximgyda10@icloud.com>
2023-03-01 16:57:29 +0200

    fix

--- src/pages/Mobile/Mobile.jsx
+++ src/pages/Mobile/Mobile.jsx
@@ -1,49 +1,49 @@
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
-import {
-  Balance,
-  TransactionsContainer,
-  MobileTypeBtn,
-  TransactionListMobile,
-} from 'components';
+// import {
+//   Balance,
+//   TransactionsContainer,
+//   MobileTypeBtn,
+//   TransactionListMobile,
+// } from 'components';
 
-import {
-  MobileContainer,
-  MobileInfo,
-  LinkToTransaction,
-  IconLink,
-} from './Mobile.styled.jsx';
-// import { changeType } from 'redux/transactions';
-// import { useDispatch } from 'react-redux';
+// import {
+//   MobileContainer,
+//   MobileInfo,
+//   LinkToTransaction,
+//   IconLink,
+// } from './Mobile.styled.jsx';
+// // import { changeType } from 'redux/transactions';
+// // import { useDispatch } from 'react-redux';
 
-export default function Mobile() {
-  const [trForm, openTrForm] = useState(false);
+// export default function Mobile() {
+//   const [trForm, openTrForm] = useState(false);
 
-  // const dispatch = useDispatch();
+//   // const dispatch = useDispatch();
 
-  // useEffect(() => {
-  //   dispatch(changeType('all'));
-  // }, []);
+//   // useEffect(() => {
+//   //   dispatch(changeType('all'));
+//   // }, []);
 
-  return (
-    <>
-      <MobileContainer>
-        {trForm ? (
-          <TransactionListMobile openTrForm={openTrForm} />
-        ) : (
-          <MobileInfo>
-            <LinkToTransaction to="/transaction">
-              <IconLink size={18} />
-              TO TRANSACTION
-            </LinkToTransaction>
+//   return (
+//     <>
+//       <MobileContainer>
+//         {trForm ? (
+//           <TransactionListMobile openTrForm={openTrForm} />
+//         ) : (
+//           <MobileInfo>
+//             <LinkToTransaction to="/transaction">
+//               <IconLink size={18} />
+//               TO TRANSACTION
+//             </LinkToTransaction>
 
-            <Balance />
-            {/* календарь */}
-          </MobileInfo>
-        )}
-        <TransactionsContainer />
-      </MobileContainer>
-      <MobileTypeBtn openTrForm={openTrForm} />
-    </>
-  );
-}
+//             <Balance />
+//             {/* календарь */}
+//           </MobileInfo>
+//         )}
+//         <TransactionsContainer />
+//       </MobileContainer>
+//       <MobileTypeBtn openTrForm={openTrForm} />
+//     </>
+//   );
+// }
