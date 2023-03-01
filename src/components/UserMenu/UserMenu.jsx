a82a9253f8a3472f3618bc67a4af3311495010e2
Nikita Sokolov <nsokolov0309@gmail.com>
2023-03-01 20:09:20 +0200

    change user menu in Mobile

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,14 +1,18 @@
 import { ModalWindow } from 'components/Modal';
 import { useAuth } from 'hooks/useAuth';
+import { useMedia } from 'hooks/useMedia';
 import { useState } from 'react';
 import Avatar from 'react-avatar';
 import { useDispatch } from 'react-redux';
 import { logOut } from 'redux/auth/authOperations';
 import { Email, ExitBtn, UserContainer } from './UserMenu.styled';
+import { RxExit } from 'react-icons/rx';
 
 export default function UserMenu() {
   const { userEmail } = useAuth();
   const [modalOpen, setModalOpen] = useState(false);
+
+  const { isMobile, isTabletAndDesktop } = useMedia();
   const dispatch = useDispatch();
 
   const handleModalOpen = () => {
@@ -32,9 +36,9 @@ export default function UserMenu() {
         round={true}
         name={userEmail}
       />
-      <Email>{userEmail}</Email>
+      {isTabletAndDesktop && <Email>{userEmail}</Email>}
       <ExitBtn type="button" onClick={handleModalOpen}>
-        Exit
+        {!isTabletAndDesktop ? <RxExit size={16} color="#CBCCD0" /> : 'Exit'}
       </ExitBtn>
       {modalOpen && (
         <ModalWindow closeModal={handleModalClose} dispatch={handleClick}>
