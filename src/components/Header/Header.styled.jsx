1f459cad1c0b8fafac5c3b3a2c155005705c39f8
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2023-03-06 18:12:21 +0100

    Update Header.styled.jsx

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const HeaderBox = styled.header`
+display: flex;
+justify-content: space-between;
+// width: 100vw;
   padding: 13px 20px 12px;
 
   @media screen and (min-width: 768px) {padding: 13px 32px 12px;}
