dc9ffa0d3daf0397f4bba2aec2931744813d0e82
Natali Poliakova <99250547+Nataliphoenix@users.noreply.github.com>
2023-03-07 19:53:09 +0200

    cat

--- src/components/CategoryContainer/CategoryContainer.jsx
+++ src/components/CategoryContainer/CategoryContainer.jsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import {
   BtnList,
   Container,
@@ -10,58 +12,60 @@ import {
 } from './CategoryContainer.styled';
 import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
 import COLORS from 'variables/colors/colors';
-import { categoryList } from 'variables/category/category';
+// import { categoryList } from 'variables/category/category';
 import IconMaker from 'components/IconMaker';
-import { useDispatch, useSelector } from 'react-redux';
+
 import {
-  changeCategoryExpenses,
-  changeCategoryIncomes,
+  // changeCategoryExpenses,
+  // changeCategoryIncomes,
   toggleReportType,
 } from 'redux/transactions/transactionsSlice';
 import {
-  selectReportsCategoryExpenses,
-  selectReportsCategoryIncomes,
-  selectSummaryByCategory,
+  // selectReportsCategoryExpenses,
+  // selectReportsCategoryIncomes,
+  // selectSummaryByCategory,
   selectTypeTransactionReports,
+ 
+  // fetchReportIncomes,
+  // selectReportExpenses,
+  getReportsData
 } from 'redux/transactions/transactionsSelectors';
+import { fetchReportExpenses} from '../../redux/transactions/transactionsOperations'
 
 export default function CategoryContainer() {
   const dispatch = useDispatch();
+
   const type = useSelector(selectTypeTransactionReports);
-  const categoryExpenses = useSelector(selectReportsCategoryExpenses);
-  const categoryIncomes = useSelector(selectReportsCategoryIncomes);
-  const fileredCategory = categoryList.filter(
-    category => category.type === type
-  );
+  console.log("type",type)
 
-  const summary = useSelector(selectSummaryByCategory);
+  useEffect(() => {
+    dispatch(fetchReportExpenses());
+  }, [dispatch]);
 
-  function handleSetCategory(name) {
-    if (type === 'Expenses') {
-      dispatch(changeCategoryExpenses(name));
-      return;
-    }
-    dispatch(changeCategoryIncomes(name));
-  }
+  const reportData = useSelector(getReportsData);
+  console.log("reportData", reportData)
+  
+  // const categoryExpenses = useSelector(selectReportsCategoryExpenses);
+  // const categoryIncomes = useSelector(selectReportsCategoryIncomes);
+  // const fileredCategory = categoryList.filter(
+  //   category => category.type === type
+  // );
 
-  function summaryCategory(name) {
-    if (summary.length === 0) {
-      return 0;
-    }
+  // const summary = useSelector(selectSummaryByCategory);
 
-    const index = summary.findIndex(
-      el =>
-        el.name.toLowerCase() === name.toLowerCase() ||
-        name.toLowerCase().includes(el.name.toLowerCase())
-    );
-    if (index === -1) {
-      return 0;
-    }
+  
 
-    return summary[index].sum;
-  }
+
+  // function handleSetCategory(name) {
+  //   if (type === 'Expenses') {
+  //     dispatch(changeCategoryExpenses(name));
+  //     return;
+  //   }
+  //   dispatch(changeCategoryIncomes(name));
+  // }
 
   return (
+     
     <Container>
       <ChoseBox>
         <ChoseBtn onClick={() => dispatch(toggleReportType())}>
@@ -74,17 +78,17 @@ export default function CategoryContainer() {
       </ChoseBox>
 
       <BtnList>
-        {fileredCategory.map(element => {
+        {reportData.map(element => {
           return (
-            <BtnListItem key={element.name}>
+            <BtnListItem key={element.id}>
               <BtnTitle
-                style={{
-                  pointerEvents:
-                    summaryCategory(element.name) > 0 ? 'auto' : 'none',
-                  opacity: summaryCategory(element.name) > 0 ? 1 : 0.5,
-                }}
+                // style={{
+                //   pointerEvents:
+                //     summaryCategory(element.name) > 0 ? 'auto' : 'none',
+                //   opacity: summaryCategory(element.name) > 0 ? 1 : 0.5,
+                // }}
               >
-                {summaryCategory(element.name)}
+                {/* {element.name} */}
               </BtnTitle>
 
               <Btn
@@ -94,28 +98,28 @@ export default function CategoryContainer() {
                   display: 'flex',
                   flexDirection: 'column',
                   alignItems: 'center',
-                  pointerEvents:
-                    summaryCategory(element.name) > 0 ? 'auto' : 'none',
-                  opacity: summaryCategory(element.name) > 0 ? 1 : 0.5,
+                  // pointerEvents:
+                  //   summaryCategory(element.name) > 0 ? 'auto' : 'none',
+                  // opacity: summaryCategory(element.name) > 0 ? 1 : 0.5,
                 }}
-                onClick={() => {
-                  handleSetCategory(element.name);
-                }}
+                // onClick={() => {
+                //   handleSetCategory(element.name);
+                // }}
               >
                 <IconMaker
-                  category={element.name}
-                  color={
-                    categoryExpenses === element.name ||
-                    categoryIncomes === element.name
-                      ? COLORS.activeColor
-                      : '#071F41'
-                  }
-                  bgColor={
-                    categoryExpenses === element.name ||
-                    categoryIncomes === element.name
-                      ? COLORS.reportsIconBg
-                      : '#F5F6FB'
-                  }
+                  category={element.id}
+                  // color={
+                  //   categoryExpenses === element.name ||
+                  //   categoryIncomes === element.name
+                  //     ? COLORS.activeColor
+                  //     : '#071F41'
+                  // }
+                  // bgColor={
+                  //   categoryExpenses === element.name ||
+                  //   categoryIncomes === element.name
+                  //     ? COLORS.reportsIconBg
+                  //     : '#F5F6FB'
+                  // }
                 />
 
                 <BtnTitle>{element.name}</BtnTitle>
@@ -127,3 +131,141 @@ export default function CategoryContainer() {
     </Container>
   );
 }
+
+
+// import {
+//   BtnList,
+//   Container,
+//   Btn,
+//   BtnTitle,
+//   ChoseBox,
+//   ChosenTitle,
+//   ChoseBtn,
+//   BtnListItem,
+// } from './CategoryContainer.styled';
+// import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+// import COLORS from 'variables/colors/colors';
+// import { categoryList } from 'variables/category/category';
+// import IconMaker from 'components/IconMaker';
+// import { useDispatch, useSelector } from 'react-redux';
+// import {
+//   changeCategoryExpenses,
+//   changeCategoryIncomes,
+//   toggleReportType,
+// } from 'redux/transactions/transactionsSlice';
+// import {
+//   selectReportsCategoryExpenses,
+//   selectReportsCategoryIncomes,
+//   selectSummaryByCategory,
+//   selectTypeTransactionReports,
+// } from 'redux/transactions/transactionsSelectors';
+
+// export default function CategoryContainer() {
+//   const dispatch = useDispatch();
+//   const type = useSelector(selectTypeTransactionReports);
+//   const categoryExpenses = useSelector(selectReportsCategoryExpenses);
+//   console.log("categoryExpenses",categoryExpenses)
+//   const categoryIncomes = useSelector(selectReportsCategoryIncomes);
+//   console.log("categoryIncomes",categoryIncomes)
+//   const fileredCategory = categoryList.filter(
+//     category => category.type === type
+//   );
+//   console.log("fileredCategory",fileredCategory)
+
+//   const summary = useSelector(selectSummaryByCategory);
+//   console.log("summary", summary)
+
+//   function handleSetCategory(name) {
+//     if (type === 'Expenses') {
+//       dispatch(changeCategoryExpenses(name));
+//       return;
+//     }
+//     dispatch(changeCategoryIncomes(name));
+//   }
+
+//   function summaryCategory(name) {
+//     if (summary.length === 0) {
+//       return 0;
+//     }
+
+//     const index = summary.findIndex(
+//       el =>
+//         el.name.toLowerCase() === name.toLowerCase() ||
+//         name.toLowerCase().includes(el.name.toLowerCase())
+//     );
+//     if (index === -1) {
+//       return 0;
+//     }
+
+//     return summary[index].sum;
+//   }
+
+//   return (
+//     <Container>
+//       <ChoseBox>
+//         <ChoseBtn onClick={() => dispatch(toggleReportType())}>
+//           <MdKeyboardArrowLeft size={20} color={COLORS.activeColor} />
+//         </ChoseBtn>
+//         <ChosenTitle>{type}</ChosenTitle>
+//         <ChoseBtn onClick={() => dispatch(toggleReportType())}>
+//           <MdKeyboardArrowRight size={20} color={COLORS.activeColor} />
+//         </ChoseBtn>
+//       </ChoseBox>
+
+//       <BtnList>
+//         {fileredCategory.map(element => {
+//           return (
+//             <BtnListItem key={element.name}>
+//               <BtnTitle
+//                 style={{
+//                   pointerEvents:
+//                     summaryCategory(element.name) > 0 ? 'auto' : 'none',
+//                   opacity: summaryCategory(element.name) > 0 ? 1 : 0.5,
+//                 }}
+//               >
+//                 {summaryCategory(element.name)}
+//               </BtnTitle>
+
+//               <Btn
+//                 style={{
+//                   width: 90,
+
+//                   display: 'flex',
+//                   flexDirection: 'column',
+//                   alignItems: 'center',
+//                   pointerEvents:
+//                     summaryCategory(element.name) > 0 ? 'auto' : 'none',
+//                   opacity: summaryCategory(element.name) > 0 ? 1 : 0.5,
+//                 }}
+//                 onClick={() => {
+//                   handleSetCategory(element.name);
+//                 }}
+//               >
+//                 <IconMaker
+//                   category={element.name}
+//                   color={
+//                     categoryExpenses === element.name ||
+//                     categoryIncomes === element.name
+//                       ? COLORS.activeColor
+//                       : '#071F41'
+//                   }
+//                   bgColor={
+//                     categoryExpenses === element.name ||
+//                     categoryIncomes === element.name
+//                       ? COLORS.reportsIconBg
+//                       : '#F5F6FB'
+//                   }
+//                 />
+
+//                 <BtnTitle>{element.name}</BtnTitle>
+//               </Btn>
+//             </BtnListItem>
+//           );
+//         })}
+//       </BtnList>
+//     </Container>
+//   );
+// }
+
+
+// transaction/reportbyexpenses?type=expenses
--- src/redux/transactions/transactionsOperations.js
+++ src/redux/transactions/transactionsOperations.js
@@ -67,9 +67,9 @@ export const fetchSummaryExpenses = createAsyncThunk(
 
 export const fetchReportExpenses = createAsyncThunk(
   'transaction/fetchSummaryInc',
-  async (_, thunkAPI) => {
+  async ({type}, thunkAPI) => {
     try {
-      const { data } = await axios.get('/transaction/reportbyexpenses');
+      const { data } = await axios.get(`/transaction/reportbyexpenses?type=${type}`);
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
