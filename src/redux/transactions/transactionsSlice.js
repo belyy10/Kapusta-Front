49681df4d6038706aa45f56931a9947f1b18238b
AlesyaMazurenko <97690121+AlesyaMazurenko@users.noreply.github.com>
2023-03-15 12:11:16 +0100

    update summary Expenses

--- src/redux/transactions/transactionsSlice.js
+++ src/redux/transactions/transactionsSlice.js
@@ -11,13 +11,7 @@ import {
 
 const initialState = {
   transactions: [],
-  summary: [
-{_id: '2023-03', expenses: -55, incomes: 8100},
-{_id: '2023-02', expenses: 680, incomes: 4000},
-    {_id: '2023-01', expenses: 0, incomes: 0 },
-  {_id: '2022-12', expenses: 0, incomes: 0 },
-  {_id: '2022-11', expenses: 0, incomes: 0 },
-  {_id: '2022-10', expenses: 0, incomes: 0}],
+  summary: [],
   reportsData: [],
   type: 'all',
   isLoading: false,
@@ -68,14 +62,12 @@ const transactionSlice = createSlice({
           ? (summary.expenses = summary.expenses - action.payload.sum)
           : (summary.incomes = summary.incomes + action.payload.sum);
       }
-      // else {
-    
-      //    (state.summary.push(action.payload.sum))
-       
-          // ? (summary.expenses = summary.expenses.push(action.payload.sum)
-          // : (summary.incomes = summary.incomes + action.payload.sum);
-        //  fetchSummaryExpenses
-      // }
+      else {
+        const dateSum = action.payload.date.slice(0,7)
+        state.mainType === 'expenses'
+          ? (state.summary = [...state.summary, { '_id': dateSum, 'expenses': action.payload.sum * -1 , 'incomes': 0}])
+          : (state.summary = [...state.summary, { '_id': dateSum, 'expenses': 0, 'incomes': action.payload.sum }])
+      }
 
     },
     changesSummaryDelete(state, action) {
